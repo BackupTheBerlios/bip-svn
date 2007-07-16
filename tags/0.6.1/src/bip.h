041a28139c52b05fc15fa78723fb3d603e28de67
nohar <nohar@8ba88fca-aef5-0310-9637-e523c8398033>
2007-07-16 19:48:43 +0000

    Initiate a 0.6.1 copy. Patch connection.c

---
+++ tags/0.6.1/src/bip.h
@@ -0,0 +1,73 @@
+/*
+ * $Id: bip.h,v 1.6 2005/04/12 19:34:35 nohar Exp $
+ *
+ * This file is part of the bip project
+ * Copyright (C) 2004 2005 Arnaud Cornet and Loïc Gomez
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
+
+#ifndef BIP_H
+#define BIP_H
+
+struct c_network
+{
+	char *name;
+#ifdef HAVE_LIBSSL
+	int ssl;
+#endif
+	list_t serverl;
+};
+
+struct c_user
+{
+	char *name;
+	unsigned char *password;
+	unsigned int seed;
+	char *default_user;
+	char *default_nick;
+	char *default_realname;
+#ifdef HAVE_LIBSSL
+	int ssl_check_mode;
+	char *ssl_check_store;
+#endif
+	list_t connectionl;
+};
+
+struct c_connection
+{
+	char *name;
+	struct c_network *network;
+	char *realname, *user, *nick;
+	char *password; 	/* server pass */
+	char *vhost;
+	unsigned short source_port;
+	list_t channell;
+
+	int follow_nick;
+	int ignore_first_nick;
+	char *away_nick;
+	list_t on_connect_send;
+	char *no_client_away_msg;
+
+	struct client *client;
+};
+
+struct c_channel
+{
+	char *name;
+	char *key;
+};
+
+#ifdef HAVE_LIBSSL
+int adm_trust(struct link_client *ic, struct line *line);
+#endif
+int adm_bip(struct link_client *ic, struct line *line, unsigned int privmsg);
+int ssl_check_trust(struct link_client *ic);
+void adm_blreset(struct link_client *ic);
+
+#endif
---
+++ tags/0.6.1/src/bipmkpw.c
@@ -0,0 +1,88 @@
+/*
+ * $Id$
+ *
+ * This file is part of the bip project
+ * Copyright (C) 2004 2005 Arnaud Cornet and Loïc Gomez
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
+
+#include "config.h"
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <errno.h>
+#include "util.h"
+#include "md5.h"
+
+int conf_log_level;
+FILE *conf_global_log_file;
+int conf_log;
+
+void readpass(char *buffer, int buflen)
+{
+	int ttyfd = open("/dev/tty", O_RDWR);
+	if (ttyfd == -1) {
+		fprintf(stderr, "Unable to open tty: %s\n", strerror(errno));
+		exit(1);
+	}
+	
+	struct termios tt, ttback;
+	memset(&ttback, 0, sizeof(ttback));
+	if (tcgetattr(ttyfd, &ttback) < 0) {
+		printf("tcgetattr failed: %s\n", strerror(errno));
+		exit(1);
+	}
+	
+	memcpy(&tt, &ttback, sizeof(ttback));
+	tt.c_lflag &= ~(ICANON|ECHO);
+	if (tcsetattr(ttyfd, TCSANOW, &tt) < 0) {
+		printf("tcsetattr failed: %s\n", strerror(errno));
+		exit(1);
+	}
+	
+	write(ttyfd, "Password: ", 10);
+	
+	int idx = 0;
+	while (idx < buflen) {
+		read(ttyfd, buffer+idx, 1);
+		if (buffer[idx] == '\n') {
+			buffer[idx] = 0;
+			break;
+		}
+		idx++;
+	}
+	
+	write(ttyfd, "\n", 1);
+	
+	tcsetattr(ttyfd, TCSANOW, &ttback);
+	close(ttyfd);
+}
+
+int main(void)
+{
+	int i;
+	static char str[256];
+	unsigned char *md5;
+	unsigned int seed;
+
+	readpass(str, 256);
+
+	// the time used to type the pass is entropy
+	srand(time(NULL));
+	seed = rand();
+	
+	md5 = chash_double(str, seed);
+        for (i = 0; i < 20; i++)
+		printf("%02x", md5[i]);
+	printf("\n");
+	free(md5);
+	return 0;
+}
